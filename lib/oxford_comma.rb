db9fc94cc1bfd784ffe4bf80aae3a0c12c141766
Nia Bert <nbert14@gmail.com>
2019-05-03 02:14:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
 if array.length == 2
    array.join(" and ")
 elsif array.length > 2
+array[-2].join("and")
 array.join(",")
-[-2].join("and")
 elsif array.length == 1
 return array.join
 end
