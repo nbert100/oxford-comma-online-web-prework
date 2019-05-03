356245b9351251f3436c50bfdc1a58a4c25d8484
Nia Bert <nbert14@gmail.com>
2019-05-03 02:02:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ if array.length == 2
    array.join(" and ")
 elsif array.length > 2
 array.join(",")
-array.insert(-2 , " and ")
+array.insert(-2 ,  and )
 elsif array.length == 1
-puts array
+return array
 end
 end
